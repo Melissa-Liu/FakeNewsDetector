9c5601c1b4cd2ad1dd150a191dd3fb123eefbbb5
Angus Ng <angus.kng@yahoo.com>
2019-04-19 14:34:23 -0400

    added assessments for varying fake percents

--- my_plot_tools.py
+++ my_plot_tools.py
@@ -67,7 +67,7 @@ def plot_coefficients(classifier, feature_names, top_features=20):
     top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
     # create plot
     plt.figure(figsize=(15, 5))
-    colors = ['red' if c < 0 else "blue" for c in coef[top_coefficients]]
+    colors = ['green' if c < 0 else "red" for c in coef[top_coefficients]]
     plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
     feature_names = np.array(feature_names)
     plt.xticks(np.arange(1, 1 + 2 * top_features), feature_names[top_coefficients], rotation=60, ha="right")
